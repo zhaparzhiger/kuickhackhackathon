4fec3b8c2c8c3740a007b2e6da6814c8a7eb3ff4
zhigazh2017@outlook.com <zhigazh2017@outlook.com>
2024-07-13 18:01:53 +0500

    Initialize

---
+++ Desktop/telegrambot/tg-web-app-react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
+function App() {
+    useEffect(() => {
+        tg.ready()
+    }, []);
+
+    const onClose = () =>{
+        tg.close()
+    }
+  return (
+    <div className="App">
+        <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
