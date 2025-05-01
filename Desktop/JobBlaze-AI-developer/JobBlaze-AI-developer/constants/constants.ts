75d85a5d7bad54b8db83ababf97d64cad39e725a
zhaparzhiger <143811531+zhaparzhiger@users.noreply.github.com>
2025-05-02 01:54:27 +0500

    Delete Desktop directory

--- Desktop/JobBlaze-AI-developer/JobBlaze-AI-developer/constants/constants.ts
+++
@@ -1,5 +0,0 @@
-export const DEEPSEEK_API_KEY = "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"
-export const VAPI_API_KEY = "a72bf6e7-3ff0-4dce-8286-638af5a39f5b"
-export const GOOGLE_API_KEY = "AIzaSyCLIB1yGy-lyyXbyWr5mebsmC46GCHx6Dk"
-
-// AIzaSyCuQ3ryOT_97gxCkrCFSCzGMSNrEl9FH9w
